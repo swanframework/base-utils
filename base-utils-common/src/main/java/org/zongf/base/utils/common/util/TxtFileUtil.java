a0c1996731547a44d6381f3fee9fef2cf215430f
zonggaofeng <zonggaofeng@autohome.com.cn>
2019-12-04 18:17:36 +0800

    抽离字符集常量

--- base-utils-common/src/main/java/org/zongf/base/utils/common/util/TxtFileUtil.java
+++ base-utils-common/src/main/java/org/zongf/base/utils/common/util/TxtFileUtil.java
@@ -1,5 +1,7 @@
 package org.zongf.base.utils.common.util;
 
+import org.zongf.base.utils.common.enums.CharsetEnum;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +14,6 @@ import java.util.function.Predicate;
  */
 public class TxtFileUtil {
 
-    /** utf-8 编码*/
-    private static String ENCODING_UTF8 = "UTF-8";
-
-    /** gbk 编码 */
-    private static String ENCODING_GBK = "GBK";
-
-    /** gb2312 编码 */
-    private static String ENCODING_GB2312 = "GB2312";
-
     /**读取文件内容, 默认以UTF-8编码打开文件
      * @param filePath 文件路径, 支持相对路径和绝对路径
      * @return 文本文件内容
@@ -29,19 +22,19 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static List<String> readFile(String filePath){
-        return readFile(filePath, line -> false, ENCODING_UTF8);
+        return readFile(filePath, line -> false, CharsetEnum.UTF8);
     }
 
     /**读取文件内容, 以指定编码格式打开文件
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 文件编码
+     * @param encodeCharset 文件编码
      * @return List<String> 文件内容
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static List<String> readFile(String filePath, String encoding){
-        return readFile(filePath, line -> false, encoding);
+    public static List<String> readFile(String filePath, CharsetEnum encodeCharset){
+        return readFile(filePath, line -> false, encodeCharset);
     }
 
     /**读取文件内容, 可忽略空行
@@ -53,7 +46,7 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static List<String> readFile(String filePath, boolean ignoreEmptyLine){
-        return readFile(filePath, line -> "".equals(line.trim()), ENCODING_UTF8);
+        return readFile(filePath, line -> "".equals(line.trim()), CharsetEnum.UTF8);
     }
 
     /**读取文件内容, 以指定编码格式打开文件, 可忽略空行
@@ -64,8 +57,8 @@ public class TxtFileUtil {
      * @author zongf
      * @created 2019-07-01
      */
-    public static List<String> readFile(String filePath, boolean ignoreEmptyLine, String encoding){
-        return readFile(filePath, line -> "".equals(line.trim()), encoding);
+    public static List<String> readFile(String filePath, boolean ignoreEmptyLine, CharsetEnum encodeCharset){
+        return readFile(filePath, line -> "".equals(line.trim()), encodeCharset);
     }
 
     /**读取文件内容, 可自定义过滤器, 过滤行. 默认以UTF-8编码打开文件
@@ -77,19 +70,19 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static List<String> readFile(String filePath, Predicate<String> filter){
-        return readFile(filePath, filter, ENCODING_UTF8);
+        return readFile(filePath, filter, CharsetEnum.UTF8);
     }
 
     /**读取文件内容.
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 设置以哪种文件编码打开文件
+     * @param encodeCharset 设置以哪种文件编码打开文件
      * @param ignoreFilter 忽略的行过滤器
      * @return List<String> 文件内容
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static List<String> readFile(String filePath, Predicate<String> ignoreFilter, String encoding) {
+    public static List<String> readFile(String filePath, Predicate<String> ignoreFilter, CharsetEnum encodeCharset) {
 
         // 创建list 存储文件内容
         List<String> contents = new ArrayList<String>();
@@ -98,7 +91,7 @@ public class TxtFileUtil {
 
         BufferedReader br = null;
         try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), encodeCharset.toString()));
 
             String line = null;
 
@@ -125,7 +118,7 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static void writeFile(List<String> contents, String filePath){
-        writeFile(contents, filePath, ENCODING_UTF8, true);
+        writeFile(contents, filePath, CharsetEnum.UTF8, true);
     }
 
     /**向文件中追加内容, 默认以UTF-8编码写入文件
@@ -137,31 +130,31 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static void writeFile(List<String> contents, String filePath, boolean overrideFile){
-        writeFile(contents, filePath, ENCODING_UTF8, overrideFile);
+        writeFile(contents, filePath, CharsetEnum.UTF8, overrideFile);
     }
 
     /**向文件中追加内容, 当文件存在时, 会进行文件覆盖
      * @param contents 文件内容
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 指定新成文件文件编码
+     * @param encodeCharset 指定新成文件文件编码
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static void writeFile(List<String> contents, String filePath, String encoding){
-        writeFile(contents, filePath, encoding, true);
+    public static void writeFile(List<String> contents, String filePath, CharsetEnum encodeCharset){
+        writeFile(contents, filePath, encodeCharset, true);
     }
 
     /**向文件中追加内容.
      * @param contents 文件内容
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 指定新成文件文件编码
+     * @param encodeCharset 指定新成文件文件编码
      * @param overrideFile 当文件已存在时, 是否进行文件覆盖
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static void writeFile(List<String> contents, String filePath, String encoding, boolean overrideFile) {
+    public static void writeFile(List<String> contents, String filePath, CharsetEnum encodeCharset, boolean overrideFile) {
 
         File file = new File(filePath);
 
@@ -172,7 +165,7 @@ public class TxtFileUtil {
 
         BufferedWriter bw = null;
         try {
-            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encodeCharset.toString()));
             for (String content : contents) {
                 bw.write(content);
                 bw.newLine();
@@ -192,7 +185,7 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static void appendFile(List<String> contents, String filePath){
-        appendFile(contents, filePath, ENCODING_UTF8, true);
+        appendFile(contents, filePath, CharsetEnum.UTF8, true);
     }
 
     /**向文件中追加内容, 默认写入文件编码为UTF-8
@@ -204,31 +197,31 @@ public class TxtFileUtil {
      * @created 2019-07-01
      */
     public static void appendFile(List<String> contents, String filePath, boolean creatNewFile){
-        appendFile(contents, filePath, ENCODING_UTF8, creatNewFile);
+        appendFile(contents, filePath, CharsetEnum.UTF8, creatNewFile);
     }
 
     /**向文件中追加内容, 当文件不存在时, 创建新文件
      * @param contents 文件内容
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 指定新成文件文件编码
+     * @param encodeCharset 指定新成文件文件编码
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static void appendFile(List<String> contents, String filePath, String encoding){
-        appendFile(contents, filePath, encoding, true);
+    public static void appendFile(List<String> contents, String filePath, CharsetEnum encodeCharset){
+        appendFile(contents, filePath, encodeCharset, true);
     }
 
     /**向文件中追加内容.
      * @param contents 文件内容
      * @param filePath 文件路径, 支持相对路径和绝对路径
-     * @param encoding 指定新成文件文件编码
+     * @param encodeCharset 指定新成文件文件编码
      * @param creatNewFile 当文件不存在时, 是否创建新的文件
      * @since 1.0
      * @author zongf
      * @created 2019-07-01
      */
-    public static void appendFile(List<String> contents, String filePath, String encoding, boolean creatNewFile) {
+    public static void appendFile(List<String> contents, String filePath, CharsetEnum encodeCharset, boolean creatNewFile) {
 
         File file = new File(filePath);
 
@@ -239,7 +232,7 @@ public class TxtFileUtil {
 
         BufferedWriter bw = null;
         try {
-            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), encoding));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), encodeCharset.toString()));
             for (String content : contents) {
                 bw.write(content);
                 bw.newLine();
